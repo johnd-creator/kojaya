d3587ddae5c4573e30b3362d001b59639fa076e7
Fauzi <fauzi.ardiyanto@gmail.com>
2026-09-07 12:58:55 +0700

    test: restore legacy ERP test discovery

--- tests/Feature/LegacyErp/BudgetControllerTest.php
+++ tests/Feature/LegacyErp/BudgetControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\LegacyErp;
 
 use App\Models\Budget;
 use App\Models\Organization;
--- tests/Feature/LegacyErp/Sprint3HrPayrollHardeningTest.php
+++ tests/Feature/LegacyErp/Sprint3HrPayrollHardeningTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\LegacyErp;
 
 use App\Models\CooperativeShuPeriod;
 use App\Models\Employee;
